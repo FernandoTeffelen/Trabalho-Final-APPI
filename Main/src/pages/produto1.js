5dd69086f8960dfc9fb8d44b2803dc56c31a6c82
Andrei-Rodrigues <andreigeral86@gmail.com>
2022-03-21 21:51:11 -0300

    produto1

---
+++ Main/src/pages/produto1.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Alert, Text, StyleSheet, View, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import mouse from '../../assets/img/mouse.jpg'; 
+
+const App = () => {
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+      <Text>
+        Tech Force
+      </Text>
+      <Image style={styles.mouseImg}
+            source = {require ('../../assets/img/mouse.jpg')}
+          />
+          <Text>sssssssssssssssssss</Text>
+          <Text>sssssssssssssssssss</Text>
+    </View>
+    </ScrollView>
+  );
+}
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#004c8b',
+    width: '100%',
+    height: '100%',
+  },
+  mouseImg: {
+    width: 350,
+    height: 350,
+  }
+})
